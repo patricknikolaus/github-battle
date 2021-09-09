8adeccfd97b059e68935bba2e27c406f13e0d068
Patrick <pdn6991@gmail.com>
2021-09-09 14:50:39 -0400

    adds popular component and styling

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
+import Popular from './components/Popular'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello Patrick!
+      <div className='container'>
+        <Popular />
       </div>
     )
   }
