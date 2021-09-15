35981860e7add11805deb719f9da00bcdecef902
Patrick <pdn6991@gmail.com>
2021-09-15 15:59:42 -0400

    adds api utility for fetching repos, and adds it to Popular.js

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,37 +1,87 @@
 import React from "react"
+import PropTypes from 'prop-types'
+import { fetchPopularRepos } from '../utils/api'
+
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+
+  return (
+    <ul className='flex-center'>
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className='btn-clear nav-link'
+            style={language === selected ? { color: 'rgb(0, 200, 40)' } : null}
+            onClick={() => onUpdateLanguage(language)}>
+            {language}
+          </button>
+        </li>
+      ))
+      }
+    </ul>
+  )
+}
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
+}
 
 export default class Popular extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null,
+      error: null,
     }
 
     this.updateLanguage = this.updateLanguage.bind(this)
+    this.isLoading = this.isLoading.bind(this)
+  }
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage)
   }
   updateLanguage(selectedLanguage) {
     this.setState({
-      selectedLanguage
+      selectedLanguage,
+      error: null,
+      repos: null
     })
+
+    fetchPopularRepos(selectedLanguage)
+      .then((repos) => this.setState({
+        repos,
+        error: null,
+      }))
+      .catch(() => {
+        console.warn('Error fetching repos: ', error)
+
+        this.setState({
+          error: 'There was an error fetching the repositories'
+        })
+      })
+  }
+  isLoading() {
+    return this.state.repos === null && this.state.error === null;
   }
   render() {
-    const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+    const { selectedLanguage, repos, error } = this.state
 
     return (
-      <ul className='flex-center'>
-        {languages.map((language) => (
-          <li key={language}>
-            <button
-              className='btn-clear nav-link'
-              style={language === this.state.selectedLanguage ? { color: 'rgb(0, 200, 40)' } : null}
-              onClick={() => this.updateLanguage(language)}>
-              {language}
-            </button>
-          </li>
-        ))
-        }
-      </ul>
+      <React.Fragment>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+      </React.Fragment>
     )
   }
 }
--- app/index.js
+++ app/index.js
@@ -16,4 +16,4 @@ class App extends React.Component {
 ReactDOM.render(
   <App />,
   document.getElementById('app')
-)
+)``
